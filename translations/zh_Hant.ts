073b995f13e49352851488468c6c41fb851b5c74
hulenkius <2024427677@qq.com>
2021-06-14 19:29:28 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: CutefishOS/Statusbar
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/statusbar/zh_Hant/

--- translations/zh_Hant.ts
+++ translations/zh_Hant.ts
@@ -6,30 +6,30 @@
     <message>
         <location filename="../qml/ControlDialog.qml" line="178"/>
         <source>Wi-Fi</source>
-        <translation type="unfinished"></translation>
+        <translation>Wi-Fi</translation>
     </message>
     <message>
         <location filename="../qml/ControlDialog.qml" line="181"/>
         <location filename="../qml/ControlDialog.qml" line="204"/>
         <source>On</source>
-        <translation type="unfinished"></translation>
+        <translation>開</translation>
     </message>
     <message>
         <location filename="../qml/ControlDialog.qml" line="181"/>
         <location filename="../qml/ControlDialog.qml" line="193"/>
         <location filename="../qml/ControlDialog.qml" line="204"/>
         <source>Off</source>
-        <translation type="unfinished"></translation>
+        <translation>關</translation>
     </message>
     <message>
         <location filename="../qml/ControlDialog.qml" line="192"/>
         <source>Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>藍芽</translation>
     </message>
     <message>
         <location filename="../qml/ControlDialog.qml" line="203"/>
         <source>Dark Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>暗黑模式</translation>
     </message>
 </context>
 </TS>
