246b86cd51430f3eed7fb69fae5deab35c26e97a
Weblate <noreply@weblate.org>
2021-06-14 17:00:16 +0200

    Added translation using Weblate (Portuguese (Brazil))

---
+++ translations/pt_BR.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>ControlDialog</name>
+    <message>
+        <location filename="../qml/ControlDialog.qml" line="178"/>
+        <source>Wi-Fi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlDialog.qml" line="181"/>
+        <location filename="../qml/ControlDialog.qml" line="204"/>
+        <source>On</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlDialog.qml" line="181"/>
+        <location filename="../qml/ControlDialog.qml" line="193"/>
+        <location filename="../qml/ControlDialog.qml" line="204"/>
+        <source>Off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlDialog.qml" line="192"/>
+        <source>Bluetooth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlDialog.qml" line="203"/>
+        <source>Dark Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
