d376cd08d394090c5cc7b3b417b0124e03c278f7
samee-np <sameerneupane92@gmail.com>
2021-06-15 09:04:43 +0000

    Translated using Weblate (Nepali)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: CutefishOS/Statusbar
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/statusbar/ne/

--- translations/ne_NP.ts
+++ translations/ne_NP.ts
@@ -6,30 +6,30 @@
     <message>
         <location filename="../qml/ControlDialog.qml" line="178"/>
         <source>Wi-Fi</source>
-        <translation type="unfinished"></translation>
+        <translation>वाइफाइ</translation>
     </message>
     <message>
         <location filename="../qml/ControlDialog.qml" line="181"/>
         <location filename="../qml/ControlDialog.qml" line="204"/>
         <source>On</source>
-        <translation type="unfinished"></translation>
+        <translation>सुचारु</translation>
     </message>
     <message>
         <location filename="../qml/ControlDialog.qml" line="181"/>
         <location filename="../qml/ControlDialog.qml" line="193"/>
         <location filename="../qml/ControlDialog.qml" line="204"/>
         <source>Off</source>
-        <translation type="unfinished"></translation>
+        <translation>बन्द</translation>
     </message>
     <message>
         <location filename="../qml/ControlDialog.qml" line="192"/>
         <source>Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>ब्लुटूथ</translation>
     </message>
     <message>
         <location filename="../qml/ControlDialog.qml" line="203"/>
         <source>Dark Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>अँध्यारो थीम</translation>
     </message>
 </context>
 </TS>
