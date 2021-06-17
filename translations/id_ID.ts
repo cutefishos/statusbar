bbc73809fccd342675b037856057b1b2064e8d4d
Muhammad Naufal Hilmy Makarim <mail@hilmy.dev>
2021-06-17 23:27:46 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: CutefishOS/Statusbar
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/statusbar/id/

--- translations/id_ID.ts
+++ translations/id_ID.ts
@@ -6,30 +6,30 @@
     <message>
         <location filename="../qml/ControlCenter.qml" line="188"/>
         <source>Wi-Fi</source>
-        <translation type="unfinished">Wi-Fi</translation>
+        <translation>Wi-Fi</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="191"/>
         <location filename="../qml/ControlCenter.qml" line="214"/>
         <source>On</source>
-        <translation type="unfinished">Hidup</translation>
+        <translation>Hidup</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="191"/>
         <location filename="../qml/ControlCenter.qml" line="203"/>
         <location filename="../qml/ControlCenter.qml" line="214"/>
         <source>Off</source>
-        <translation type="unfinished">Mati</translation>
+        <translation>Mati</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="202"/>
         <source>Bluetooth</source>
-        <translation type="unfinished">Bluetooth</translation>
+        <translation>Bluetooth</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="213"/>
         <source>Dark Mode</source>
-        <translation type="unfinished">Mode Gelap</translation>
+        <translation>Mode Gelap</translation>
     </message>
 </context>
 <context>
@@ -60,7 +60,7 @@
     <message>
         <location filename="../qml/main.qml" line="65"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Tutup</translation>
     </message>
 </context>
 </TS>
