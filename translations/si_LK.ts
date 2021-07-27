33ed6e9ae83284c587744eddce5d309980c6802e
Hosted Weblate <hosted@weblate.org>
2021-07-27 16:54:24 +0200

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 100.0% (6 of 6 strings)
    
    Translated using Weblate (Sinhala)
    
    Currently translated at 83.3% (5 of 6 strings)
    
    Added translation using Weblate (Sinhala)
    
    Co-authored-by: Deleted User <noreply+34039@weblate.org>
    Co-authored-by: HelaBasa <R45XvezA@protonmail.ch>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/statusbar/si/
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/statusbar/zh_Hant/
    Translation: CutefishOS/Statusbar

--- translations/si_LK.ts
+++ translations/si_LK.ts
@@ -1,35 +1,35 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_Hant">
+<TS version="2.1" language="si">
 <context>
     <name>ControlCenter</name>
     <message>
         <location filename="../qml/ControlCenter.qml" line="188"/>
         <source>Wi-Fi</source>
-        <translation>無線網路</translation>
+        <translation>වයි-ෆයි</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="191"/>
         <location filename="../qml/ControlCenter.qml" line="214"/>
         <source>On</source>
-        <translation>開</translation>
+        <translation>සක්‍රීයයි</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="191"/>
         <location filename="../qml/ControlCenter.qml" line="203"/>
         <location filename="../qml/ControlCenter.qml" line="214"/>
         <source>Off</source>
-        <translation>關</translation>
+        <translation>අක්‍රීයයි</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="202"/>
         <source>Bluetooth</source>
-        <translation>藍芽</translation>
+        <translation>බ්ලූටූත්</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="213"/>
         <source>Dark Mode</source>
-        <translation>暗黑模式</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -37,7 +37,7 @@
     <message>
         <location filename="../qml/main.qml" line="71"/>
         <source>Close</source>
-        <translation>關閉</translation>
+        <translation>වසන්න</translation>
     </message>
 </context>
 </TS>
